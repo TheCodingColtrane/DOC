c44cdc8fa331425f556923c28784f4a91d9d3043
Weverson Rafael <92547812+TheCodingColtrane@users.noreply.github.com>
2021-10-14 22:58:40 -0300

    Update and rename p.cs to TimeStampDeserializer.cs

---
+++ api/Util/TimeStampDeserializer.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
+namespace DOC.Util
+{
+    public class TimeStampDeserializer: JsonConverter<TimeSpan>
+    {
+        public override TimeSpan Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return TimeSpan.ParseExact(reader.GetString(), "c", CultureInfo.InvariantCulture);
+        }
+
+        public override void Write(Utf8JsonWriter writer, TimeSpan value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString("c", CultureInfo.InvariantCulture));
+        }
+    }
+}
+
+
